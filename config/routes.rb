aeef3476ff75c49a56c6395024c4a80e0e0c1bc4
christopheleray <christophe.leray1@gmail.com>
2019-01-21 19:46:53 +0100

    update routes for root issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope ":locale", locale: /fr|en/ do
+  get '/', to: 'static_pages#home'
+  scope ':locale', locale: /fr|en/ do
     get 'contact', to: 'static_pages#contact'
     get 'about', to: 'static_pages#about'
     root to: 'static_pages#home'
   end
+
 end
