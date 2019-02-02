7cf2e591028f6edc970b4bc41f7f44a34e18ed05
christopheleray <christophe.leray1@gmail.com>
2019-02-02 11:51:00 +0100

    add views and setting for langauge update

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module MultiLanguageI18n
     config.load_defaults 5.2
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.fallbacks = true
-    config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
