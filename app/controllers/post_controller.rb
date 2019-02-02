b3e15acf5a8ddcf2bbd1c875c455fb7859c00141
christopheleray <christophe.leray1@gmail.com>
2019-02-02 13:14:51 +0100

    add CRUD post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,35 @@
+# frozen_string_literal: true
+
 class PostController < ApplicationController
-  def new
+  def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    post.valid?(@post)
+    redirect_to user_post_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def post_valid?(post)
+    if post.valid?
+      post.save
+      flash[:notice] = ""
+    else
+      flash[:alert] = post.errors.full_messages
   end
 end
