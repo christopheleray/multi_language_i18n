d16fa2f57f0af811f1f0129b80647688cc045a91
christopheleray <christophe.leray1@gmail.com>
2019-02-04 10:03:53 +0100

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Post.delete_all
+User.delete_all
+5.times do |i|
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: "password",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    locale: "fr")
+    p "User fr #{i} : créé"      
+end
+
+  5.times do |i|
+    User.create!(
+      email: Faker::Internet.unique.email,
+      password: "password",
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      locale: "en")
+    p "User en #{i} : créé"                  
+  end
+
+50.times do |i|
+  Post.create!(
+    title: Faker::FamilyGuy.character, 
+    content: Faker::FamilyGuy.quote, 
+    user_id: Faker::Number.between(User.first.id,User.last.id))
+  p "Post #{i} : créé"      
+end  
