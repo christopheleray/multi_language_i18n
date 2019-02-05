ed3e52d8506c6e502ef5fff9f237171abdfd14ba
christopheleray <christophe.leray1@gmail.com>
2019-02-05 12:25:56 +0100

    update user migration

--- db/migrate/20190123100852_devise_create_users.rb
+++ db/migrate/20190123100852_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :locale, default: "fr"
-      t.string :frist_name
+      t.string :first_name
       t.string :last_name
       ## Recoverable
       t.string   :reset_password_token
